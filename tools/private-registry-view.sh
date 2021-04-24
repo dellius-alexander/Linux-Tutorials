4dcf8eac810dbab5f19af1c1609a8a0110f12052
* <*>
2021-04-23 23:48:26 -0400

    list repos

--- tools/private-registry-view.sh
+++ tools/private-registry-view.sh
@@ -1,39 +1,99 @@
-#!/bin/bash
-#	view-private-registry-remote  2.3.2.184  2021-04-10T13:48:30.105938-05:00 (CDT)  https://github.com/BradleyA/Search-docker-registry-v2-script  master  uadmin  six-rpi3b.cptx86.com 2.3.1-2-gb873c33  
-#	   view-private-registry view-private-registry-remote -->   new release 2.3.2  
-#	view-private-registry-remote  v2.2.42  2019-02-18T23:12:08.838659-06:00 (CST)  https://github.com/BradleyA/Search-docker-registry-v2-script.git  uadmin  two.cptx86.com v2.1  
-#	   add License and mark file with markit 
+#!/usr/bin/env bash
+##########################################################################
+#  curl -X GET --user dalexander:$(echo -n ${REGISTRY_PASS} | base64 -d) https://registry.dellius.app/v2/_catalog 
+# Registry Password
+if [ ${#REGISTRY_PASS} -gt 0 ]; then
+REGISTRY_PASS=$(echo -n ${REGISTRY_PASS} | base64 -d)  # Variable must be a base64 --encoded {hash}
+fi
+# Enter remote registtry v2 server IP address or FQDN
+# Setup environmental variables for each of these values:
 #
-### view-private-registry
-#       Copyright (c) 2021 Bradley Allen
-#       License is in the online DOCUMENTATION, see github
-### view-private-registry-remote
-#       View private registry v2 from remote host
-#
-#       Enter remote registtry v2 server IP address or FQDN
-REMOTE_REGISTRY_HOST="${__REMOTE_PRIVATE_REGISTRY__}"
+REMOTE_REGISTRY_HOST="${REMOTE_PRIVATE_REGISTRY}"
 #       Enter remote registry v2 ssh admin account for the above host
-REMOTE_ADMIN_ACCOUNT="${USER}"
-if [ "$1" == "--help" ];
-then
-        echo -e "\nUsage:  view-private-registry-remote [REMOTE_REGISTRY_HOST] [REMOTE_ADMIN_ACCOUNT]"
-        echo -e "\nDisplay remote private registry."
-	echo -e "\nDefault:\n\tREMOTE_REGISTRY_HOST=$REMOTE_REGISTRY_HOST"
-	echo -e "\tREMOTE_ADMIN_ACCOUNT=$REMOTE_ADMIN_ACCOUNT"
-        echo -e "\nOptions:\n\t--help	Print usage\n"
-	exit
-fi
-if [ ! -z $1 ];
-then
+REMOTE_ADMIN_ACCOUNT="${REMOTE_ADMIN_ACCOUNT}"
+##########################################################################
+_list(){
+        RESULTS="${1}"
+        cnt=0
+        #RESULTS=${RESULTS[@]}  # Checking formatted values
+        IFS=',' read -ra splitIFS<<<${RESULTS[@]}
+        #echo ${#splitIFS[@]}   # Checking array count
+        for c in ${splitIFS[@]}; 
+        do
+                if [[ $c =~ 'repositories' ]]; then
+                        #c=($(echo $c))
+                        printf "\n${c:0:12}:\n"
+                        echo "-------------"
+                        echo  -n ${c:13}
+                        continue
+                fi                
+                if [[ $cnt -lt $((${#splitIFS[@]}))  ]]; then
+                        printf "\n$c "                
+                fi
+                cnt=$((cnt+1))
+        done
+        echo ""
+        IFS=''
+        exit 0
+}
+##########################################################################
+_list_repo(){
+        cnt=0
+        IFS=',' read -ra splitIFS<<<${RESULTS[@]}
+        #echo ${#splitIFS[@]}   # Checking array count
+        for c in ${splitIFS[@]}; 
+        do
+                if [[ $cnt -lt 1 ]]; then
+                        printf "\n$c \n"
+                fi
+                if [ $cnt -gt 0 ] && [ $cnt -lt $((${#splitIFS[@]} - 1)) ]; then
+                        echo -n "$c, "
+                elif [ $cnt -eq $((${#splitIFS[@]} -1)) ]; then
+                        printf "$c \n" | tr -d ','
+                fi
+                cnt=$((cnt+1))
+        done
+        IFS=''
+        exit 0
+
+}
+
+##########################################################################
+if [[ "$1" =~ ^(--help)$ ]]; then
+        printf "\nUsage:  ${0} [REMOTE_REGISTRY_HOST] [REMOTE_ADMIN_ACCOUNT]\n"
+        printf "\nDisplay remote private registry. \nCreate an environmental variable for each to bypass setting it manually.\n"
+	printf "\nDefault:\n\tREMOTE_REGISTRY_HOST=$REMOTE_REGISTRY_HOST\n"
+	printf "\n\tREMOTE_ADMIN_ACCOUNT=$REMOTE_ADMIN_ACCOUNT\n"
+        printf "\nOptions:\n\t--help	This menu\n"
+        printf "\n\t--list - lists contents of your registry\n"
+        printf "\n\t--list <some repo name in your registry>"
+	exit 0
+elif [[ "$2" =~ ^(http://|https://)[a-zA-Z0-9_./]+(.com|.net|.org|.io)?[a-zA-Z0-9_.]$ ]]; then
 #       Set remote registtry v2 server IP address or FQDN
-	REMOTE_REGISTRY_HOST="$1"
-        echo "REMOTE_REGISTRY_HOST=$1"
+	REMOTE_REGISTRY_HOST="$2"
+        printf "\n\nREMOTE_REGISTRY_HOST: ${REMOTE_REGISTRY_HOST}\n"
 fi
-if [ ! -z $2 ];
-then
+if [[ "$1" =~ [a-zA-Z0-9]+ ]] && [[ "$1" =~ !^(--list)$ ]]; then
 #       Set remote registry v2 ssh admin account for the above host
-	REMOTE_ADMIN_ACCOUNT="$2"
-        echo "REMOTE_ADMIN_ACCOUNT=$2"
+	REMOTE_ADMIN_ACCOUNT="$1"
+        printf "\\nREMOTE_ADMIN_ACCOUNT: ${REMOTE_ADMIN_ACCOUNT}\n"
 fi
 ##
-ssh $REMOTE_ADMIN_ACCOUNT@$REMOTE_REGISTRY_HOST /usr/local/bin/view-private-registry
+if [[ "$1" =~ ^(--list)$ ]] || [[ "$3" =~ ^(--list)$ ]] ; then
+        if [[ "$1" =~ ^(--list)$ ]] && [ -z $2 ]; then
+        RESULTS=($(curl -X GET --user ${REMOTE_ADMIN_ACCOUNT}:${REGISTRY_PASS} ${REMOTE_REGISTRY_HOST}/v2/_catalog 2>/dev/null))
+        RESULTS=($(echo ${RESULTS} | tr -d '{' | tr -d '}' | tr -d '[' | tr -d ']' | tr -d '\"'))
+        _list ${RESULTS[@]}
+        elif [[ "$1" =~ ^(--list)$ ]] && [ ! -z $2 ]; then
+        RESULTS=($(curl -X GET --user ${REMOTE_ADMIN_ACCOUNT}:${REGISTRY_PASS} ${REMOTE_REGISTRY_HOST}/v2/${2}/tags/list 2>/dev/null))
+        RESULTS=($(echo ${RESULTS} | tr -d '{' | tr -d '}' | tr -d '[' | tr -d ']' | tr -d '\"'))
+        _list_repo ${RESULTS[@]}
+        fi
+        #${string:position:length}
+
+
+        
+        #RESULTS=${RESULTS[@]}  # Checking formatted values
+
+fi
+printf "\nUsage: ${0} --help\n"
